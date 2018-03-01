82095db77c55ea3e3331856c2490b0f3abbcd36c
TheOtherDevProject <flakeskivlog@gmail.com>
2018-03-01 10:44:59 +0900

    No sleep and no sleep makes developer bad pathy

--- alloy-editor.js
+++ alloy-editor.js
@@ -1,3 +1,3 @@
 'use strict';
-const AlloyEditor = require('../node_modules/alloyeditor/dist/alloy-editor/alloy-editor-all.js');
+const AlloyEditor = require('../alloyeditor/dist/alloy-editor/alloy-editor-all.js');
 module.exports = AlloyEditor;
